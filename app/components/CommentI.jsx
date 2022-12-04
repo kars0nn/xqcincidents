d161b02d14a39c63f70e414a6baae966eb466b8d
Karson Durocher <karson@durochers.org>
2022-12-03 20:04:56 -0500

    d

--- app/components/CommentI.jsx
+++ app/components/CommentI.jsx
@@ -2,30 +2,31 @@ import { useEffect, useState } from 'react'
 import reactStringReplace from 'react-string-replace'
 // import { nazi, nword } from 'expletives'
 
-export function nword(text) {
-    const r = new RegExp(`[nN]+[iI1lL|]+[GgKkQq469]+[Ee3Aa4iI]+[RrAa4]+[sS]?`)
-    return r.test(text)
-}
-export function nazi(text) {
-    const r = new RegExp(`[nN]+[a4A|]+[zZ]+[Ii1l]+[sS]?`)
-    return r.test(text)
-}
-
 export function CommentI({ emotes, content }) {
-    let emotesFound = []
-    emotes.forEach((e) => {
-        if (content.toString().split(" ").includes(e.name)) {
-            emotesFound.push({ name: e.name, url: `https://cdn.7tv.app/emote/${e.id}/1x.webp`, width: e.width, height: e.height })
-        } else {
+    // let emotesFound = []
+    // emotes.forEach((e) => {
+    //     if (content.toString().split(" ").includes(e.name)) {
+    //         emotesFound.push({ name: e.name, url: `https://cdn.7tv.app/emote/${e.id}/1x.webp`, width: e.width, height: e.height })
+    //     } else {
 
-        }
-    })
+    //     }
+    // })
 
-    emotesFound.forEach((e) => {
-        content = reactStringReplace(content, e.name, (match, index) => (
-            <img title={e.name} className={`inline-block`} src={e.url} width={e.width.join(", ")} height={e.height.join(", ")} />
-        ))
-    })
+    function nword(text) {
+        const r = new RegExp(`[nN]+[iI1lL|]+[GgKkQq469]+[Ee3Aa4iI]+[RrAa4]+[sS]?`)
+        return r.test(text)
+    }
+    function nazi(text) {
+        const r = new RegExp(`[nN]+[a4A|]+[zZ]+[Ii1l]+[sS]?`)
+        return r.test(text)
+    }
+    
+
+    // emotesFound.forEach((e) => {
+    //     content = reactStringReplace(content, e.name, (match, index) => (
+    //         <img title={e.name} className={`inline-block`} src={e.url} width={e.width.join(", ")} height={e.height.join(", ")} />
+    //     ))
+    // })
 
     function nword(text) {
         const r = new RegExp(`[nN]+[iI1lL|]+[GgKkQq469]+[Ee3Aa4iI]+[RrAa4]+[sS]?`)
